988d021f12ddcd6222750e0b3f3a58bd93cf962e
Kei-141 <ruler141@gmail.com>
2021-09-30 14:31:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
 
+options = Options()
+#options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.youtube.com/watch?v=X9zw0QF12Kc")
